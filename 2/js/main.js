abff0fbdfbbcb62f9ae5bf821adfa0caded5869e
Kosorukov <vendetta.koc@gmail.com>
2022-09-20 10:11:10 +0300

    process

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,38 +1,10 @@
-// const products = [
-//     {id: 1, title: 'Notebook', price: 2000},
-//     {id: 2, title: 'Mouse', price: 20},
-//     {id: 3, title: 'Keyboard', price: 200},
-//     {id: 4, title: 'Gamepad', price: 50}
-// ];
-// //Функция для формирования верстки каждого товара
-// const renderProduct = (product,img='https://via.placeholder.com/200x150') => {
-//     return `<div class="product-item">
-//                 <img src="${img}">
-//                 <h3>${product.title}</h3>
-//                 <p>${product.price}</p>
-//                 <button class="buy-btn">Купить</button>
-//             </div>`
-// };
-// const renderPage = list => {
-//     document.querySelector('.products').innerHTML =
-//         (list.map(product => renderProduct(product))).join('');
-// };
-
-
-
-// renderPage(products);
-
-class ProductList  {
-    constructor(catalog = '.products') {
-        this.catalog = catalog;
+class GoodsList  {
+    constructor(goodsList = '.products') {
+        this.goodsList = goodsList;
         this.goods = [];
-        this.addGoods(); //наполняем массив товаров при создании объекта класса
-        this.renderCatalog(); //создаём карточки в html
     }
 
-//Продумайте, какие методы понадобятся для работы с этими сущностями.
-
-    addGoods(){
+    fetchGoods(){
         this.goods = [
         {id: 1, title: 'Notebook', price: 2000},
         {id: 2, title: 'Mouse', price: 20},
@@ -40,33 +12,31 @@ class ProductList  {
         {id: 4, title: 'Gamepad', price: 50}
     ]
     }
-    
+
     renderCatalog(){
-        const catalog = document.querySelector(this.catalog);
-        for (let good of this.goods) {
-            const product = new Product(good); 
-            // теперь понял про практику обращения Учителя к методам Доктора
-            catalog.insertAdjacentHTML('beforeend', product.renderProduct());
-        }
+        let listHTML = '';
+        this.goods.forEach(good => {
+            const product = new GoodsItem(good); 
+            listHTML += product.renderGoodsItem();
+        });
+        document.querySelector(this.goodsList).innerHTML = listHTML;
     }
 
-//Добавьте для GoodsList метод, определяющий суммарную стоимость всех товаров.
-
-    getSumAllGoods(){
+    getSumPriceAllGoods(){
         let totalPrice = 0;
         this.goods.forEach(good => totalPrice += Number(good.price));
         return totalPrice;
     }
 }
 
-class Product {
+class GoodsItem {
     constructor(product,img='https://via.placeholder.com/200x150') {
         this.id = product.id;
         this.title = product.title;
         this.price = product.price;
         this.img = img;
     }
-    renderProduct(){
+    renderGoodsItem(){
         return `<div class="product-item">
                  <img src="${this.img}">
                  <h3>${this.title}</h3>
@@ -76,5 +46,7 @@ class Product {
     }
 }
 
-let catalog = new ProductList();
-console.log(catalog.getSumAllGoods());
+let goodsList = new GoodsList();
+goodsList.fetchGoods();
+goodsList.renderCatalog();
+console.log(goodsList.getSumPriceAllGoods());
